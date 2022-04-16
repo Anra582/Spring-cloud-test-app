0ab47f7b44fd7b916770a19a857086808eb52912
AZhvakin <anra582@gmail.com>
2022-04-16 22:05:09 +0700

    Initial commit

---
+++ configserver/src/test/java/com/anradev/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anradev.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
