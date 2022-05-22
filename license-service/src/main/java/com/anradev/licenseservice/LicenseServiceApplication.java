9d5a09e33cc036c447f6895cf0c5b577fca61662
AZhvakin <anra582@gmail.com>
2022-05-22 19:31:49 +0700

    Predicate and filter factories for Gateway server. RestTemplate interceptor for forward propagation UserContext

--- license-service/src/main/java/com/anradev/licenseservice/LicenseServiceApplication.java
+++ license-service/src/main/java/com/anradev/licenseservice/LicenseServiceApplication.java
@@ -1,14 +1,19 @@
 package com.anradev.licenseservice;
 
+import com.anradev.licenseservice.utils.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 @SpringBootApplication
@@ -26,5 +31,22 @@ public class LicenseServiceApplication {
 		messageSource.setUseCodeAsDefaultMessage(true);
 		messageSource.setBasenames("messages");
 		return messageSource;
+	}
+
+	@SuppressWarnings("unchecked")
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate(){
+		RestTemplate template = new RestTemplate();
+		List interceptors = template.getInterceptors();
+		if (interceptors==null){
+			template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		}
+		else{
+			interceptors.add(new UserContextInterceptor());
+			template.setInterceptors(interceptors);
+		}
+
+		return template;
 	}
 }
