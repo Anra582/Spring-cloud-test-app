222e40c5007778bc2750f7b3acbc010a9e5e2efe
AZhvakin <anra582@gmail.com>
2022-07-15 19:53:48 +0700

    init zookeeper, kafka and redis

--- organization-service/src/main/java/com/anradev/organization/utils/UserContext.java
+++ organization-service/src/main/java/com/anradev/organization/utils/UserContext.java
@@ -1,5 +1,8 @@
 package com.anradev.organization.utils;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,36 +12,28 @@ public class UserContext {
     public static final String USER_ID        = "tmx-user-id";
     public static final String ORGANIZATION_ID = "tmx-organization-id";
 
-    private String correlationId= new String();
-    private String authToken= new String();
-    private String userId = new String();
-    private String organizationId = new String();
+    private static final ThreadLocal<String> correlationId = new ThreadLocal<>();
+    private static final ThreadLocal<String> authToken = new ThreadLocal<>();
+    private static final ThreadLocal<String> userId = new ThreadLocal<>();
+    private static final ThreadLocal<String> organizationId = new ThreadLocal<>();
 
-    public String getCorrelationId() { return correlationId;}
-    public void setCorrelationId(String correlationId) {
-        this.correlationId = correlationId;
+
+    public static String getCorrelationId() { return correlationId.get(); }
+    public static void setCorrelationId(String cid) {correlationId.set(cid);}
+
+    public static String getAuthToken() { return authToken.get(); }
+    public static void setAuthToken(String aToken) {authToken.set(aToken);}
+
+    public static String getUserId() { return userId.get(); }
+    public static void setUserId(String aUser) {userId.set(aUser);}
+
+    public static String getOrgId() { return organizationId.get(); }
+    public static void setOrgId(String aOrg) {organizationId.set(aOrg);}
+
+    public static HttpHeaders getHttpHeaders(){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(CORRELATION_ID, getCorrelationId());
+
+        return httpHeaders;
     }
-
-    public String getAuthToken() {
-        return authToken;
-    }
-
-    public void setAuthToken(String authToken) {
-        this.authToken = authToken;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-	public String getOrganizationId() {
-		return organizationId;
-	}
-	public void setOrganizationId(String organizationId) {
-		this.organizationId = organizationId;
-	}
-
 }
