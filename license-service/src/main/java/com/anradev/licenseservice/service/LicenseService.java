ac20154c0592365319971686056cf04e28d6f5e6
AZhvakin <anra582@gmail.com>
2025-01-15 21:24:37 +0700

    Keycloak image version update. Fixed issue with unable to auto import keycloak-realm on start

--- license-service/src/main/java/com/anradev/licenseservice/service/LicenseService.java
+++ license-service/src/main/java/com/anradev/licenseservice/service/LicenseService.java
@@ -12,8 +12,6 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.MessageSource;
